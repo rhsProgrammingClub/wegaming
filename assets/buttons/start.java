a499fc4baf854c079915e28299f973b1a48c1396
SpaceFox2050 <87918634+SpaceFox2050@users.noreply.github.com>
2021-11-25 16:02:12 -0800

    matthew messed everything up

---
+++ assets/buttons/start.java
@@ -0,0 +1,8 @@
+import ky;
+import java.awt.Font;
+
+public static start extends Text{
+  String text = "start";
+  Font xfont = new Font("TimesRoman", Font.BOLD, 18);
+  //rushed unfinished text 
+}  
