66c5eea7f1859429431b975293bb840c48e9e0ce
SpaceFox2050 <87918634+SpaceFox2050@users.noreply.github.com>
2022-01-29 18:35:31 -0800

    created ground
    covers ablt and ult icons

--- src/Scenes/GameScene/Ground.java
+++ src/Scenes/GameScene/Ground.java
@@ -1,5 +1,6 @@
 import ky.Asset;
 import ky.CollisionEntity;
+import ky.Vector2D;
 
 public class Ground extends CollisionEntity {
 
@@ -7,11 +8,15 @@ public class Ground extends CollisionEntity {
 
     public Ground(double x, double y, int collisionBoxWidth, int collisionBoxHeight, String name) {
         super(x, y, collisionBoxWidth, collisionBoxHeight, 3, name);
-        // groundAsset = new Asset("assets/ground.png", new Vector2D(0, 0), 2);
-        // groundAsset.setVisible(true);
-        // groundAsset.rescale(1920/Main.width);
-        // add(groundAsset);
+         //groundAsset = new Asset("assets/ground.png", new Vector2D(0, 0), 2);
+         //groundAsset.setVisible(true);
+         //groundAsset.rescale(1920/Main.width);
+         //add(groundAsset);
         setVisible(true);
+
+        groundAsset = new Asset("assets/ground.png", new Vector2D(0, 0), collisionBoxWidth, collisionBoxHeight, 0);
+        groundAsset.setVisible(true);
+        add(groundAsset);
     }
 
     Asset getAsset() {
